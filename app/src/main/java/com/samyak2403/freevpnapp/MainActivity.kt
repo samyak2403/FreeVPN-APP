4f2a704d7be221c9de3bc4b2f865b803b506a05b
Samyak <samyakkamble6659@gmail.com>
2024-12-30 14:11:09 +0530

    Initial commit

---
+++ app/src/main/java/com/samyak2403/freevpnapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.samyak2403.freevpnapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.samyak2403.freevpnapp.Activitys.HomeActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        // Add a delay to navigate to MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish() // Close SplashActivity so it won't appear on back press
+        }, 3000) // Delay in milliseconds (3 seconds)
+
+
+
+    }
+}
